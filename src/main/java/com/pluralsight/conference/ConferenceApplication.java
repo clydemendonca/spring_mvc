9af7a05db7fcd64716808190b5d4247b6d1a6947
Clyde Mendonca <clyde.mendonca01@gmail.com>
2020-03-02 15:47:02 +0530

    Index File modified

--- src/main/java/com/pluralsight/conference/ConferenceApplication.java
+++ src/main/java/com/pluralsight/conference/ConferenceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConferenceApplication {
+public class ConferenceApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceApplication.class, args);
